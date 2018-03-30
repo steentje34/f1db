17a716e58fccfd3cb0cb5a0053ab1b55dadd931f
Simon Vanderschaeghe <steentje34@users.noreply.github.com>
2018-03-30 13:44:36 +0200

    Fixed Include\classes.php

--- Include/classes.php
+++ Include/classes.php
@@ -1,14 +1,16 @@
 <?php
 
-require_once '../Classes/Circuit.php';
-require_once '../Classes/Constructor.php';
-require_once '../Classes/Driver.php';
-require_once '../Classes/Engine.php';
-require_once '../Classes/Event.php';
-require_once '../Classes/FastestLap.php';
-require_once '../Classes/Nationality.php';
-require_once '../Classes/Point.php';
-require_once '../Classes/Pole.php';
-require_once '../Classes/Race.php';
-require_once '../Classes/Season.php';
-require_once '../Classes/Win.php';
+$path_classes = __DIR__ . '/../Classes/';
+
+require_once $path_classes . 'Circuit.php';
+require_once $path_classes . 'Constructor.php';
+require_once $path_classes . 'Driver.php';
+require_once $path_classes . 'Engine.php';
+require_once $path_classes . 'Event.php';
+require_once $path_classes . 'FastestLap.php';
+require_once $path_classes . 'Nationality.php';
+require_once $path_classes . 'Point.php';
+require_once $path_classes . 'Pole.php';
+require_once $path_classes . 'Race.php';
+require_once $path_classes . 'Season.php';
+require_once $path_classes . 'Win.php';
