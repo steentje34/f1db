ddb8a35a5397d4f2c0defe6520464efc99d86846
Simon Vanderschaeghe <steentje34@users.noreply.github.com>
2018-06-22 14:45:55 +0200

    Added summary methods to Driver class

--- Classes/Driver.php
+++ Classes/Driver.php
@@ -31,7 +31,7 @@ class Driver {
         $this->death_date = $result['death_date'];
     }
 
-    function calculateAge() {
+    function calculateAge(): int {
         $from = new DateTime($this->birth_date);
         if ($this->death_date !== NULL){
             $to = new DateTime($this->death_date);
@@ -42,15 +42,82 @@ class Driver {
         return $from->diff($to)->y;
     }
     
-    function getChampionships() {
-        $query = "SELECT count(driver_id) AS count FROM seasons WHERE driver_id=? GROUP BY driver_id";
+    function getChampionships(): int {
+        $query = "SELECT count(*) AS count FROM seasons WHERE driver_id=?";
         $stmt = $this->conn->prepare($query);
         $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $championships = $result['count'];
+        $championships = (is_null($result['count']) ? 0 : $result['count']);
 
         return $championships;
+    }
+
+    function getPointsAwarded(): int {
+        $query = "SELECT ifnull(sum(driver_points), 0) AS sum FROM points WHERE driver_id=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result['sum'];
+    }
+
+    function getPointsDiscarded(): int {
+        $query = "SELECT ifnull(sum(driver_points_discarded), 0) AS sum FROM points WHERE driver_id=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result['sum'];
+    }
+
+    function getPointsTotal(): int {
+        $query = "SELECT ifnull(sum(driver_points), 0) + ifnull(sum(driver_points_discarded), 0) AS sum FROM points WHERE driver_id=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result['sum'];
+    }
+
+    function getWins(): int {
+        $query = "SELECT count(*) AS count FROM wins WHERE driver_id=? OR driver_2_id=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
+        $stmt->bindValue(2, $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $wins = (is_null($result['count']) ? 0 : $result['count']);
+
+        return $wins;
+    }
+
+    function getPoles(): int {
+        $query = "SELECT count(*) AS count FROM poles WHERE driver_id=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $poles = (is_null($result['count']) ? 0 : $result['count']);
+
+        return $poles;
+    }
+
+    function getFastestLaps(): int {
+        $query = "SELECT count(*) AS count FROM fastest_laps WHERE driver_id=?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(1, $this->id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $fastestLaps = (is_null($result['count']) ? 0 : $result['count']);
+
+        return $fastestLaps;
     }
 }
